f3ca64d26dd6d8b1e03b10a1b8604a5031841be2
JeremyBoh <jeremybohannon@outlook.com>
2016-04-04 03:08:55 -0400

    Fixed variable bug

--- relief.js
+++ relief.js
@@ -1,24 +1,25 @@
 window.onload = function(){
 
   var defaultColor = "lightgrey";
-  var elements;
+  var elements, i, len;
 
-  elements = document.getElementsByClassName("fbChatSidebar"), i, len;
+  elements = document.getElementsByClassName("fbChatSidebar");
 
   document.body.style.background = defaultColor;
+
   document.getElementById("contentCol").style.backgroundColor = defaultColor;
 
   for(i = 0, len = elements.length; i < len; i++){
   elements[i].style.backgroundColor = defaultColor;
   };
 
-  elements = document.getElementsByClassName("_4-u2"), i, len;
+  elements = document.getElementsByClassName("_4-u2");
 
   for(i = 0, len = elements.length; i < len; i++){
   elements[i].style.backgroundColor = defaultColor;
   };
 
-  elements = document.getElementsByClassName("_4-u8"), i, len;
+  elements = document.getElementsByClassName("_4-u8");
 
   for(i = 0, len = elements.length; i < len; i++){
   elements[i].style.backgroundColor = defaultColor;
