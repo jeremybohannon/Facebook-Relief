33a2dbcca86eacc2d19a993b920c260f87d03a2a
JeremyBoh <jeremybohannon@outlook.com>
2016-04-04 02:59:11 -0400

    first commit

---
+++ relief.js
@@ -0,0 +1,29 @@
+window.onload = function(){
+
+
+var elements = document.getElementsByClassName("fbChatSidebar"), i, len;
+var defaultColor = "lightgrey";
+
+
+
+document.body.style.background = defaultColor;
+document.getElementById("contentCol").style.backgroundColor = defaultColor;
+
+for(i = 0, len = elements.length; i < len; i++){
+  elements[i].style.backgroundColor = defaultColor;
+};
+
+var elements = document.getElementsByClassName("_4-u2"), i, len;
+
+for(i = 0, len = elements.length; i < len; i++){
+  elements[i].style.backgroundColor = defaultColor;
+};
+
+var elements = document.getElementsByClassName("_4-u8"), i, len;
+
+for(i = 0, len = elements.length; i < len; i++){
+  elements[i].style.backgroundColor = defaultColor;
+};
+
+
+};
