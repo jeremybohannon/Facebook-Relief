c7096617aae028ba7ae674f85946138f37624e0a
JeremyBoh <jeremybohannon@outlook.com>
2016-04-06 23:57:06 -0400

    Updated personal feeds and on page change reload

--- relief.js
+++ relief.js
@@ -4,40 +4,51 @@ window.onload = function(){
   window.defaultBackgroundColor = "#676565";
   window.defaultInnerBackgroundColor = "#424040";
 
-  var elements, i, len;
-
+  window.currentUrl = window.location.href;
 
   defaultLoad();
 
   window.onscroll = function(){updateFeed()};
 
+  setInterval(function(){
+    if(currentUrl != window.location.href){
+      defaultLoad();
+    }
+
+
+  }, 3000);
+
 };
 
 function defaultLoad(){
-  elements = document.getElementsByClassName("fbChatSidebar");
 
   document.body.style.background = defaultBackgroundColor;
 
   document.getElementById("contentCol").style.backgroundColor = defaultInnerBackgroundColor;
 
+  document.getElementById("pagelet_timeline_recent").style.backgroundColor = defaultBackgroundColor;
   updateFeed();
 };
 
 
 function updateFeed(){
+
+  loopThroughElements("fbChatSidebar", defaultColor);
+
+  loopThroughElements("_4-u2", defaultColor);
+
+  loopThroughElements("_4-u8", defaultColor);
+
+  loopThroughElements("_4arz", "#F6F7F8");
+
+};
+
+function loopThroughElements(className, classColor){
+  var elements, i, len;
+  elements = document.getElementsByClassName(className);
+
   for(i = 0, len = elements.length; i < len; i++){
-  elements[i].style.backgroundColor = defaultColor;
+    elements[i].style.backgroundColor = classColor;
   };
 
-  elements = document.getElementsByClassName("_4-u2");
-
-  for(i = 0, len = elements.length; i < len; i++){
-  elements[i].style.backgroundColor = defaultColor;
-  };
-
-  elements = document.getElementsByClassName("_4-u8");
-
-  for(i = 0, len = elements.length; i < len; i++){
-  elements[i].style.backgroundColor = defaultColor;
-  };
 };
